61c823d6cd0f41b77eb7605ddadd719ab24191b3
Felipe Henrique De Oliveira <felipehenrique0207@gmail.com>
2025-08-22 11:11:04 -0300

    refactor: Ajusta a saída dos números pares e ímpares
    - Adiciona `end=' '` nos `print` para exibir os números na mesma linha.
    - Arquivo: des47

--- Python - cursoemvideo/Mundo 2/Desafios/des47.py
+++ Python - cursoemvideo/Mundo 2/Desafios/des47.py
@@ -2,8 +2,8 @@ from time import sleep
 print('\n\033[1:40mEsses são todos os número pares de 0 a 50:\033[m\n')
 for par in range(0, 51, 2):
     sleep(0.05)
-    print(par)
+    print(par, end=' ')
 print('\n\033[1:40mEsses são todos os número ínpares de 0 a 50:\033[m\n')
 for impar in range(1, 51, 2):
     sleep(0.05)
-    print(impar)
+    print(impar, end=' ')
