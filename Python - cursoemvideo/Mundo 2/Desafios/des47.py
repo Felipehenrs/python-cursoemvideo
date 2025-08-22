6f2a5f118584eaed8d76ea5a7b05dfc34ba1712d
Felipe Henrique De Oliveira <felipehenrique0207@gmail.com>
2025-08-22 11:06:44 -0300

    feat: Adiciona programa que exibe números pares e ímpares
    Cria um script que usa laços de repetição para exibir números.
    O programa:
    - Itera de 0 a 50 para exibir os números pares e ímpares separadamente.
    - Utiliza a função `sleep()` para criar um efeito de contagem.
    - Adiciona cores para organizar a saída do terminal.
    Arquivo: des47

---
+++ Python - cursoemvideo/Mundo 2/Desafios/des47.py
@@ -0,0 +1,9 @@
+from time import sleep
+print('\n\033[1:40mEsses são todos os número pares de 0 a 50:\033[m\n')
+for par in range(0, 51, 2):
+    sleep(0.05)
+    print(par)
+print('\n\033[1:40mEsses são todos os número ínpares de 0 a 50:\033[m\n')
+for impar in range(1, 51, 2):
+    sleep(0.05)
+    print(impar)
