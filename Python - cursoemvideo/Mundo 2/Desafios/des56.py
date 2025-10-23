3a0827b1d16911b35dee0b32a0a179d793d965c4
Felipehenrs <felipehenrique0207@gmail.com>
2025-10-23 11:28:36 -0300

    feat: calcular média de idade, homem mais velho e mulheres com menos de 20 anos
    - Lê nome, idade e sexo de 4 pessoas
    - Calcula média de idade do grupo
    - Identifica o homem mais velho e exibe o nome
    - Conta quantas mulheres têm menos de 20 anos
    - Exibe o resumo final formatado

---
+++ Python - cursoemvideo/Mundo 2/Desafios/des56.py
@@ -0,0 +1,24 @@
+mediaidade = 0
+idadehomens = 0
+mulheresmaisnovas = 0
+
+for quantidade_pessoas in range (1, 5):
+    pessoa = str(input(f'Digite o nome da {quantidade_pessoas} pessoa: '))
+    idade = int(input(f'Digite a idade dessa pessoa: '))
+    sexo = int(input(f'Digite o sexo dessa pessoa. (Digite "1" para masculino e "2" para feminino) -> '))
+    mediaidade += idade
+    if sexo == 1 and idade >= idadehomens:
+        idadehomens = idade
+        maisvelho = pessoa
+    elif sexo == 2 and idade < 20:
+        mulheresmaisnovas += 1
+
+print(f'A média da idade do grupo é {mediaidade / 4}, o nome do homem mais velho é {maisvelho} e a '
+      f'quantidade de mulheres mais novas que 20 anos é {mulheresmaisnovas}.')
+
+
+
+
+
+
+
