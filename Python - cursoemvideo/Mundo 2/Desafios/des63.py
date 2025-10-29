068ab572dbded6a3560c9c9eeffc13fba29dc0b6
Felipehenrs <felipehenrique0207@gmail.com>
2025-10-28 23:18:02 -0300

    feat: sequência de Fibonacci com controle de termos dinâmico
    - Implementa geração da sequência de Fibonacci com número de termos definido pelo usuário
    - Exibe os dois primeiros termos fixos (0 e 1) e calcula os seguintes iterativamente
    - Utiliza contador para limitar a quantidade de termos exibidos
    - Atualiza os valores de t1, t2 e t3 a cada iteração garantindo a sequência correta

---
+++ Python - cursoemvideo/Mundo 2/Desafios/des63.py
@@ -0,0 +1,12 @@
+n = int(input('Quantos termos quer mostrar da sequência de fibonacci? '))
+contador = 0
+t1 = 0
+t2 = 1
+t3 = t1 + t2
+print(f'{t1} -> {t2}', end='')
+while contador != n - 2:
+    contador += 1
+    print(f' -> {t3}', end='')
+    t1 = t2
+    t2 = t3
+    t3 = t1 + t2
