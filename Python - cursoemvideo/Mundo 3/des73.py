7ee7c16c95cd544a112c15c4db34d3cdc418fd16
Felipehenrs <felipehenrique0207@gmail.com>
2025-11-06 22:05:06 -0300

    Implementa um programa que manipula uma tupla contendo os 20 times do Campeonato Brasileiro 2025.
    - Exibe os quatro últimos colocados da tabela.
    - Mostra os times em ordem alfabética usando sorted().
    - Identifica a posição específica do Vasco da Gama dentro da tupla.
    - Utiliza slicing, index() e f-strings para exibição dinâmica dos resultados.

---
+++ Python - cursoemvideo/Mundo 3/des73.py
@@ -0,0 +1,7 @@
+times = ('Palmeiras', 'Flamengo', 'Cruzeiro', 'Mirassol', 'Bahia', 'Botafogo', 'Fluminense', 'São Paulo', 'Vasco da Gama', 'Corinthians', 'Atlético-MG', 'Red Bull Bragantino', 'Grêmio', 'Ceará', 'Internacional', 'Vitória', 'Santos', 'Juventude', 'Fortaleza', 'Sport')
+
+print(f'\nOs últimos 4 times colocados da tabela são {times[16:]}')
+
+print(f'\nA ordem alfabética dos times do brasileirão é {sorted(times)}')
+
+print(f'\nO Vasco está na {times.index('Vasco da Gama') + 1}º posição.')
