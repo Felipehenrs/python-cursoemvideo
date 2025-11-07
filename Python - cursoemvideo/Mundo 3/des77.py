6b5ae5338bf488451a1c374e9197254c56b68ce5
Felipehenrs <felipehenrique0207@gmail.com>
2025-11-07 12:18:24 -0300

    Projetando...

---
+++ Python - cursoemvideo/Mundo 3/des77.py
@@ -0,0 +1,5 @@
+palavras = ('aprender', 'programar', 'linguagem', 'python', 'curso', 'grátis', 'estudar', 'práticar', 'trabalhar', 'mercado', 'programador', 'futuro')
+vogais = ('a' 'e', 'i', 'o', 'u')
+for palavra in palavras:
+    if vogais is palavra:
+        print(vogais.index(palavra))
